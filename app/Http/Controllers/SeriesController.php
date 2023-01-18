a8939f05c11241953cc0cecf6a66b952c549e625
SaorCampos <SaorCampos>
2023-01-18 17:45:54 -0300

    começando relacionamentos de tabelas

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Http\Requests\SeriesFormRequest;
 
 class SeriesController extends Controller
 {
@@ -19,7 +20,7 @@ class SeriesController extends Controller
     {
         return view('series.create');
     }
-    public function store(Request $request)
+    public function store(SeriesFormRequest $request)
     {
         $serie = Serie::create($request->all());
         return to_route('series.index')->with('mensagem.sucesso',"Série '{$serie->nome}' adicionada com sucesso");
@@ -33,7 +34,7 @@ class SeriesController extends Controller
     {
         return view('series.edit')->with('series', $series);
     }
-    public function update(Serie $series, Request $request)
+    public function update(Serie $series, SeriesFormRequest $request)
     {
         $series->fill($request->all());
         $series->save();
--- resources/views/components/series/form.blade.php
+++ resources/views/components/series/form.blade.php
@@ -1,11 +1,11 @@
     <form action="{{$action}}" method="post">
         @csrf {{-- valida as informações enviadas pelo formulario --}}
-        @isset($nome)
+        @if($update)
             @method('PUT')
         @endisset
         <div class="mb-3">
             <label for="nome" class="form-label">Nome</label>
-            <input type="text" name="nome" id="nome" class="form-control" @isset($nome) value="{{$nome}}"@endisset>
+            <input type="text" name="nome" id="nome" class="form-control" @isset($nome) value="{{$nome}}"@endif>
         </div>
         <button type="submit" class="btn btn-primary">Adicionar</button>
         <a href="{{route('series.index')}}" class="btn btn-secondary">Voltar</a>
